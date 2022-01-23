582fe961f1ec2dd235e686480cbce3efde90ac59
Martin Schaer <martin@schaerweb.com>
2022-01-23 14:08:43 -0600

    Format files

--- js/plugins.js
+++ js/plugins.js
@@ -1,17 +1,39 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+(function plugins() {
+  let method;
+  const noop = function noop() {};
+  const methods = [
+    "assert",
+    "clear",
+    "count",
+    "debug",
+    "dir",
+    "dirxml",
+    "error",
+    "exception",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "markTimeline",
+    "profile",
+    "profileEnd",
+    "table",
+    "time",
+    "timeEnd",
+    "timeline",
+    "timelineEnd",
+    "timeStamp",
+    "trace",
+    "warn",
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+  let { length } = methods;
+  // eslint-disable-next-line no-multi-assign
+  const console = (window.console = window.console || {});
 
-  while (length--) {
+  while (length) {
+    length -= 1;
     method = methods[length];
 
     // Only stub undefined methods.
@@ -19,6 +41,6 @@
       console[method] = noop;
     }
   }
-}());
+})();
 
 // Place any jQuery/helper plugins in here.
